b9f4b9f884c9d1275673e976316dc2c8557988ec
turku <turkucakmak@hotmail.com>
2023-05-31 01:10:15 +0300

    Competed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Mama",
+    streetAddress : "BarişManço"
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+const newobj= {...employee};
+newobj[key] = value;
+
+return newobj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const newobj= {...employee};
+    delete newobj[key];
+    return newobj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
